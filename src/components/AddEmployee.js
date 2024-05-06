865114032b26ffd87018c849c6c15d6f72541b51
Anthony Villegas <villegasanthony87@yahoo.com>
2024-05-06 00:03:22 -0500

    button layout changed on quick form

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -31,12 +31,31 @@ export const AddEmployee = () => {
                 </div>
 
                 <div className="items-center justify-center h-14 w-full my-4 space-x-64 pt-4">
-                    <button className="rounded text-white font-semibold py-2 px-2 bg-gray-500 hover:bg-gray-800">
-                        Save
+                    {/*<button className="rounded text-white font-semibold py-2 px-2 bg-gray-500 hover:bg-gray-800">*/}
+                    {/*    Save*/}
+                    {/*</button>*/}
+
+                    <button className="group relative">
+                        <div
+                            className="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-green-400 px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-active:translate-x-0 group-active:translate-y-0">
+                            Save
+                        </div>
+                        <div
+                            className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5] group-active:translate-x-0 group-active:translate-y-0 group-active:shadow-none"></div>
                     </button>
-                    <button className="rounded text-white font-semibold py-2 px-2 bg-red-400 hover:bg-red-700">
-                        Clear
+
+                    <button className="group relative">
+                        <div className="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-red-500 px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-active:translate-x-0 group-active:translate-y-0">
+                            Clear
+                        </div>
+                        <div
+                            className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5] group-active:translate-x-0 group-active:translate-y-0 group-active:shadow-none"></div>
                     </button>
+
+
+                    {/*<button className="rounded text-white font-semibold py-2 px-2 bg-red-400 hover:bg-red-700">*/}
+                    {/*    Clear*/}
+                    {/*</button>*/}
                 </div>
             </div>
         </div>
